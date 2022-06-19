a6de76ce74f978ca70d42ff19c6d978f3415c539
Raicuparta <raicuparta@gmail.com>
2022-06-19 13:16:45 +0200

    Cleanup

--- ShipbreakerVr/XrLoader.cs
+++ ShipbreakerVr/XrLoader.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
 using UnityEngine.XR.Management;
-using UnityEngine.XR.OpenXR;
-using Valve.VR;
 
 namespace ShipbreakerVr;
 
@@ -12,42 +8,7 @@ public class XrLoader: MonoBehaviour
 {
 	private void Awake()
 	{
-		// SteamVR_Actions.PreInitialize();
 		LoadXRModule();
-
-		SteamVR.Initialize();
-		SteamVR.settings.pauseGameWhenDashboardVisible = false;
-
-		// InputTracking.disablePositionalTracking = true;
-		
-		if (XRGeneralSettings.Instance != null && XRGeneralSettings.Instance.Manager != null
-		                                       && XRGeneralSettings.Instance.Manager.activeLoader != null)
-		{
-			// XRGeneralSettings.Instance.Manager.StartSubsystems();
-		}
-		else
-			throw new Exception("Cannot initialize VRSubsystem");
-
-		//Change tracking origin to headset
-		var subsystems = new List<XRInputSubsystem>();
-		SubsystemManager.GetInstances(subsystems);
-		foreach (var subsystem in subsystems)
-		{
-			subsystem.TrySetTrackingOriginMode(TrackingOriginModeFlags.Device);
-			subsystem.TryRecenter();
-		}
-	}
-
-	private void Update()
-	{
-		// if (Input.GetKeyDown(KeyCode.KeypadMinus))
-		// {
-		// 	Camera.main.transform.parent.localScale *= 1.1f;
-		// }
-		// if (Input.GetKeyDown(KeyCode.KeypadPlus))
-		// {
-		// 	Camera.main.transform.parent.localScale *= 0.9f;
-		// }
 	}
 
 	private static void LoadXRModule()
@@ -64,18 +25,6 @@ public class XrLoader: MonoBehaviour
 		if (xrManagerSettings == null) throw new Exception("XRManagerSettings instance is null");
 
 		xrManagerSettings.InitializeLoaderSync();
-		if (xrManagerSettings.activeLoader == null) throw new Exception("Cannot initialize OpenVR Loader");
-
-		var openXrSettings = ScriptableObject.FindObjectOfType<OpenXRSettings>(true);
-		
-		Debug.Log($"### Found OpenXrSettinghs? {openXrSettings != null}");
-
-		// var openVrSettings = OpenVRSettings.GetSettings(false);
-		// openVrSettings.EditorAppKey = "steam.app.753640";
-		// openVrSettings.InitializationType = OpenVRSettings.InitializationTypes.Scene;
-		// openVrSettings.StereoRenderingMode = OpenVRSettings.StereoRenderingModes.SinglePassInstanced;
-		// if (openVrSettings == null) throw new Exception("OpenVRSettings instance is null");
-		//
-		// openVrSettings.SetMirrorViewMode(OpenVRSettings.MirrorViewModes.Right);
+		if (xrManagerSettings.activeLoader == null) throw new Exception("Cannot initialize OpenXR Loader");
 	}
 }
