e5a96f9f525cd3b6f5b3e86cba0325f272070bce
Raicuparta <raicuparta@gmail.com>
2022-06-21 11:46:20 +0200

    quality buttons

--- ShipbreakerVr/Plugin.cs
+++ ShipbreakerVr/Plugin.cs
@@ -1,20 +1,43 @@
 ﻿using System.Reflection;
 using BepInEx;
 using HarmonyLib;
+using UnityEngine;
 
-namespace ShipbreakerVr
+namespace ShipbreakerVr;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
 {
-	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-	public class Plugin : BaseUnityPlugin
-	{
-		// TODO find a good way to check this.
-		private void Awake()
-		{
-			Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+    private bool lights = true;
 
-			// Plugin startup logic
-			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
-			gameObject.AddComponent<ModXrManager>();
-		}
-	}
+    private bool shadows = true;
+
+    // TODO find a good way to check this.
+    private void Awake()
+    {
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        gameObject.AddComponent<ModXrManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.F5))
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha0)) QualitySettings.SetQualityLevel(0);
+            if (Input.GetKeyDown(KeyCode.Alpha1)) QualitySettings.SetQualityLevel(1);
+            if (Input.GetKeyDown(KeyCode.Alpha2)) QualitySettings.SetQualityLevel(2);
+            if (Input.GetKeyDown(KeyCode.Alpha3)) QualitySettings.SetQualityLevel(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F6))
+        {
+            lights = !lights;
+
+            foreach (var light in FindObjectsOfType<Light>())
+                light.enabled = lights;
+        }
+    }
 }
