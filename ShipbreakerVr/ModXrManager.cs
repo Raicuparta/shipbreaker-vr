b906f485ef379a01a97631de898d798521b91e6a
Raicuparta <raicuparta@gmail.com>
2022-06-21 07:52:27 +0200

    Somewhat usable HUD

--- ShipbreakerVr/ModXrManager.cs
+++ ShipbreakerVr/ModXrManager.cs
@@ -1,56 +1,78 @@
 ﻿using System;
+using BBI.Unity.Game;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using UnityEngine.XR.Management;
 using UnityEngine.XR.OpenXR;
 
 namespace ShipbreakerVr;
 
-public class ModXrManager: MonoBehaviour
+public class ModXrManager : MonoBehaviour
 {
-	public static bool IsVrEnabled;
-	private static OpenXRLoaderBase openXrLoader;
-	private static bool IsInitialized => openXrLoader != null && openXrLoader.GetValue<bool>("isInitialized");
+    public static bool IsVrEnabled;
+    private static OpenXRLoaderBase openXrLoader;
+    private static bool IsInitialized => openXrLoader != null && openXrLoader.GetValue<bool>("isInitialized");
 
-	private void Awake()
-	{
-		var xrManagerBundle = VrAssetManager.LoadBundle("xrmanager");
+    private void Awake()
+    {
+        var xrManagerBundle = VrAssetManager.LoadBundle("xrmanager");
 
-		foreach (var xrManager in xrManagerBundle.LoadAllAssets())
-			Debug.Log($"######## Loaded xrManager: {xrManager.name}");
+        foreach (var xrManager in xrManagerBundle.LoadAllAssets())
+            Debug.Log($"######## Loaded xrManager: {xrManager.name}");
 
-		var instance = XRGeneralSettings.Instance;
-		if (instance == null) throw new Exception("XRGeneralSettings instance is null");
+        var instance = XRGeneralSettings.Instance;
+        if (instance == null) throw new Exception("XRGeneralSettings instance is null");
 
-		var xrManagerSettings = instance.Manager;
-		if (xrManagerSettings == null) throw new Exception("XRManagerSettings instance is null");
+        var xrManagerSettings = instance.Manager;
+        if (xrManagerSettings == null) throw new Exception("XRManagerSettings instance is null");
 
-		xrManagerSettings.InitializeLoaderSync();
-		if (xrManagerSettings.activeLoader == null) throw new Exception("Cannot initialize OpenVR Loader");
+        xrManagerSettings.InitializeLoaderSync();
+        if (xrManagerSettings.activeLoader == null) throw new Exception("Cannot initialize OpenVR Loader");
 
-		openXrLoader = xrManagerSettings.ActiveLoaderAs<OpenXRLoaderBase>();
+        openXrLoader = xrManagerSettings.ActiveLoaderAs<OpenXRLoaderBase>();
 
-		// Reference OpenXRSettings just to make this work.
-		// TODO figure out how to do this properly.
-		OpenXRSettings unused;
-	}
+        // Reference OpenXRSettings just to make this work.
+        // TODO figure out how to do this properly.
+        OpenXRSettings unused;
+    }
 
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.F3))
-		{
-			if (!IsVrEnabled)
-			{
-				XRGeneralSettings.Instance.Manager.StartSubsystems();
-				XRGeneralSettings.Instance.Manager.activeLoader.Initialize();
-				XRGeneralSettings.Instance.Manager.activeLoader.Start();
-			}
-			else
-			{
-				XRGeneralSettings.Instance.Manager.activeLoader.Stop();
-				XRGeneralSettings.Instance.Manager.activeLoader.Deinitialize();
-			}
-			IsVrEnabled = IsInitialized;
-		}
-	}
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            if (!IsVrEnabled)
+            {
+                XRGeneralSettings.Instance.Manager.StartSubsystems();
+                XRGeneralSettings.Instance.Manager.activeLoader.Initialize();
+                XRGeneralSettings.Instance.Manager.activeLoader.Start();
+                SetUpUi(true);
+            }
+            else
+            {
+                XRGeneralSettings.Instance.Manager.activeLoader.Stop();
+                XRGeneralSettings.Instance.Manager.activeLoader.Deinitialize();
+                SetUpUi(false);
+            }
+
+            IsVrEnabled = IsInitialized;
+        }
+    }
+
+    private void SetUpUi(bool isVr)
+    {
+        var hudSystem = FindObjectOfType<GlassModeController>();
+        if (!hudSystem) Debug.LogError("Failed to find HUD System");
+        hudSystem.transform.SetParent(Camera.main.transform, false);
+        hudSystem.transform.localPosition = Vector3.forward * 3;
+        hudSystem.transform.localScale = Vector3.one * 0.004f;
+
+        var hudCanvasOther = hudSystem.transform.Find("HUDContainer/HUD Canvas - Other/").GetComponent<Canvas>();
+        hudCanvasOther.renderMode = isVr ? RenderMode.WorldSpace : RenderMode.ScreenSpaceOverlay;
+        hudCanvasOther.transform.localPosition = Vector3.zero;
+        hudCanvasOther.transform.localRotation = Quaternion.identity;
+        hudCanvasOther.transform.localScale = Vector3.one;
+
+        hudCanvasOther.GetComponent<CanvasScalerImprover>().enabled = !isVr;
+        hudCanvasOther.GetComponent<CanvasScaler>().enabled = !isVr;
+    }
 }
--- ShipbreakerVr/VrCamera.cs
+++ ShipbreakerVr/VrCamera.cs
@@ -1,33 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.SpatialTracking;
-using UnityEngine.XR.Management;
 
 namespace ShipbreakerVr;
 
-public class VrCamera: MonoBehaviour
+public class VrCamera : MonoBehaviour
 {
-	private Camera mainCamera;
-	private Camera vrCamera;
-	
-	public static void Create(Camera mainCamera)
-	{
-		var instance = new GameObject("VrCamera").AddComponent<VrCamera>();
-		instance.transform.SetParent(mainCamera.transform, false);
-		instance.mainCamera = mainCamera;
-		
-		instance.vrCamera = instance.gameObject.AddComponent<Camera>();
-		instance.vrCamera.farClipPlane = mainCamera.farClipPlane;
-		instance.vrCamera.nearClipPlane = mainCamera.nearClipPlane;
-		instance.vrCamera.cullingMask = mainCamera.cullingMask;
-		instance.vrCamera.transform.localPosition = Vector3.forward * 0.2f;
-		
-		var poseDriver = instance.gameObject.AddComponent<TrackedPoseDriver>();
-		poseDriver.trackingType = TrackedPoseDriver.TrackingType.RotationOnly;
-	}
+    private Camera mainCamera;
+    private Camera vrCamera;
 
-	private void Update()
-	{
-		mainCamera.enabled = !ModXrManager.IsVrEnabled;
-		vrCamera.enabled = ModXrManager.IsVrEnabled;
-	}
+    public static void Create(Camera mainCamera)
+    {
+        var instance = new GameObject("VrCamera").AddComponent<VrCamera>();
+        instance.transform.SetParent(mainCamera.transform, false);
+        instance.mainCamera = mainCamera;
+
+        instance.vrCamera = instance.gameObject.AddComponent<Camera>();
+        instance.vrCamera.farClipPlane = mainCamera.farClipPlane;
+        instance.vrCamera.nearClipPlane = mainCamera.nearClipPlane;
+        instance.vrCamera.cullingMask = mainCamera.cullingMask;
+        instance.vrCamera.transform.localPosition = Vector3.forward * 0.2f;
+        instance.vrCamera.tag = "MainCamera";
+
+        var poseDriver = instance.gameObject.AddComponent<TrackedPoseDriver>();
+        poseDriver.trackingType = TrackedPoseDriver.TrackingType.RotationOnly;
+    }
+
+    private void Update()
+    {
+        mainCamera.enabled = !ModXrManager.IsVrEnabled;
+        vrCamera.enabled = ModXrManager.IsVrEnabled;
+    }
 }
