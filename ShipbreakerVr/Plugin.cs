7751fb623a9a3ed7ba29c8779cb4d1402d2454ad
Raicuparta <raicuparta@gmail.com>
2022-06-21 17:13:20 +0200

    Bump version 0.2.0

--- ShipbreakerVr/Plugin.cs
+++ ShipbreakerVr/Plugin.cs
@@ -5,30 +5,21 @@ using UnityEngine;
 
 namespace ShipbreakerVr;
 
-[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInPlugin("ShipbreakerVr", "ShipbreakerVr", "0.2.0")]
 public class Plugin : BaseUnityPlugin
 {
     private bool lights = true;
 
-    // TODO find a good way to check this.
     private void Awake()
     {
         Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
 
-        // Plugin startup logic
-        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         gameObject.AddComponent<ModXrManager>();
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.F5))
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha0)) QualitySettings.SetQualityLevel(0);
-            if (Input.GetKeyDown(KeyCode.Alpha1)) QualitySettings.SetQualityLevel(1);
-            if (Input.GetKeyDown(KeyCode.Alpha2)) QualitySettings.SetQualityLevel(2);
-            if (Input.GetKeyDown(KeyCode.Alpha3)) QualitySettings.SetQualityLevel(3);
-        }
+        if (Input.GetKeyDown(KeyCode.F5)) QualitySettings.SetQualityLevel(0);
 
         if (Input.GetKeyDown(KeyCode.F6))
         {
