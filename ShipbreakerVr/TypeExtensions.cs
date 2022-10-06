9fa43145a82cac7549268a83bf107e79d9b1dbeb
Raicuparta <raicuparta@gmail.com>
2022-10-06 23:34:17 +0200

    Cleanup

--- ShipbreakerVr/TypeExtensions.cs
+++ ShipbreakerVr/TypeExtensions.cs
@@ -9,14 +9,7 @@ public static class TypeExtensions
     private const BindingFlags Flags = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public |
                                        BindingFlags.Static;
 
-    public static MethodInfo GetAnyMethod(this Type type, string name)
-    {
-        return type.GetMethod(name, Flags) ??
-               type.BaseType?.GetMethod(name, Flags) ??
-               type.BaseType?.BaseType?.GetMethod(name, Flags);
-    }
-
-    public static MemberInfo GetAnyMember(this Type type, string name)
+    private static MemberInfo GetAnyMember(this Type type, string name)
     {
         return type.GetMember(name, Flags).FirstOrDefault() ??
                type.BaseType?.GetMember(name, Flags).FirstOrDefault() ??
@@ -31,33 +24,5 @@ public static class TypeExtensions
             PropertyInfo property => (T)property.GetValue(obj, null),
             _ => default
         };
-    }
-
-    public static void SetValue(this object obj, string name, object value)
-    {
-        switch (obj.GetType().GetAnyMember(name))
-        {
-            case FieldInfo field:
-                field.SetValue(obj, value);
-                break;
-            case PropertyInfo property:
-                property.SetValue(obj, value, null);
-                break;
-        }
-    }
-
-    public static void Invoke(this object obj, string name, params object[] parameters)
-    {
-        Invoke<object>(obj, name, parameters);
-    }
-
-    public static T Invoke<T>(this object obj, string name, params object[] parameters)
-    {
-        return (T)obj.GetType().GetAnyMethod(name)?.Invoke(obj, parameters);
-    }
-
-    public static T Invoke<T>(this Type type, string name, params object[] parameters)
-    {
-        return (T)type.GetAnyMethod(name).Invoke(null, parameters);
     }
 }
